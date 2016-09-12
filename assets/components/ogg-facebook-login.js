eaa0df6c408855d26d3c8a90137fdd01b0167d8a
Ogg (Valentin Padurean) <valentin.padurean@bitsoftware.ro>
2016-09-12 10:55:59 +0300

    Add icon to FB login button

--- assets/components/ogg-facebook-login.js
+++ assets/components/ogg-facebook-login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import FacebookLogin from 'react-facebook-login';
+import IoSocialFacebook from 'react-icons/lib/io/social-facebook';
 
 const responseFacebook = (response) => {
   console.log(response);
@@ -13,12 +14,18 @@ export default class OggFacebookLogin extends Component {
     };
   }
   render() {
+    const style = {
+      padding: "0 0 .25rem 0",
+      margin: "0 .5rem 0 0",
+    };
     return (
       <FacebookLogin
         appId="688661967935072"
-        autoLoad={true}
+        autoLoad={false}
         fields="name,email,picture"
-        callback={responseFacebook} />
+        callback={responseFacebook}
+        icon={<IoSocialFacebook size={24} style={style} />}
+        size="small" />
     );
   }
 
